46793087f164893fa219b9a276baba99be5a634c
gempir <gempir.dev@gmail.com>
2025-07-26 19:53:14 +0200

    basic plugin

--- src/test/java/com/gempir/RunemersionPluginTest.java
+++ src/test/java/com/gempir/RunemersionPluginTest.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.gempir;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class RunemersionPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
